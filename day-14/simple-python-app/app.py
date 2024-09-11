044bee7328b3a514a8221bce47c0283c8100f804
gunasimhareddy72 <67228880+gunasimhareddy72@users.noreply.github.com>
2024-09-11 21:08:08 +0100

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, gsr namaste! how are you'
+    return 'Hello, bagunnara bagunnara'
 
 if __name__ == '__main__':
     app.run()
